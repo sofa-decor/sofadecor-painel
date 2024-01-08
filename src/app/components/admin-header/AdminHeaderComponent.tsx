9fdfdb31b897a5fe48505e9f74c403e1120fe317
Mateus Rossetto <mateusc.rossetto@hotmail.com>
2024-01-08 17:21:55 -0300

    feat: add menu into admin header - products and users

--- src/app/components/admin-header/AdminHeaderComponent.tsx
+++ src/app/components/admin-header/AdminHeaderComponent.tsx
@@ -1,6 +1,8 @@
-import { Stack, Typography } from "@mui/material";
+import { Stack, Tab, Tabs, Typography } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { SyntheticEvent, useState } from "react";
 import Logo from "../../../assets/logo.png";
+import useAppRouterHook from "../../../hooks/useAppRouterHook";
 import appColors from "../../colors/appColors";
 import { HeaderContainer } from "./AdminHeaderStyles";
 
@@ -18,6 +20,15 @@ const headerTheme = createTheme({
 });
 
 export default function AdminHeaderComponent() {
+    const [tabValue, setTabValue] = useState("produtos");
+    const { router } = useAppRouterHook();
+
+    const handleChangeTab = (e: SyntheticEvent, value: string) => {
+        e.preventDefault();
+        if (value == "sobre") router.about.go();
+        setTabValue(value);
+    };
+
     return (
         <ThemeProvider theme={headerTheme}>
             <HeaderContainer>
@@ -26,6 +37,12 @@ export default function AdminHeaderComponent() {
                         SOFÁ DECOR
                     </Typography>
                     <img width={35} height={35} src={Logo} alt="logo" />
+                </Stack>
+                <Stack direction="row" flex={1} justifyContent="center">
+                    <Tabs value={tabValue} onChange={handleChangeTab}>
+                        <Tab label="Produtos" value="produtos" />
+                        <Tab label="Usuários" value="usuários" />
+                    </Tabs>
                 </Stack>
             </HeaderContainer>
         </ThemeProvider>
